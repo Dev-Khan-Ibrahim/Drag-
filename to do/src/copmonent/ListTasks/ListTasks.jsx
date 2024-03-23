32869e6f735e963e2b8744cc6101f54b9536512b
Dev Ibrahim <devibrahimkhan@email.com>
2024-03-24 02:11:04 +0600

    Initial commit

--- to do/src/copmonent/ListTasks/ListTasks.jsx
+++ to do/src/copmonent/ListTasks/ListTasks.jsx
@@ -1,28 +1,31 @@
 import Section from "../section/Section";
-import "./Listtask.css"
+import "./Listtask.css";
 const ListTasks = ({ tasks, setTasks }) => {
-
   const statues = ["Todo", "Inprogress", "Completed"];
 
-  
-    return (
-      <div className="Fainal_section">  <div className="sectiuon_statues" >
+  return (
+    <div className="Fainal_section">
+      {" "}
+      <div className="sectiuon_statues">
         {statues.map((status, index) => (
-          <Section key={index} status={status}   counter={tasks.filter(task => task.status === status).length} />
+          <Section
+            key={index}
+            status={status}
+            counter={tasks.filter((task) => task.status === status).length}
+          />
         ))}
-      <div className="section Todo">
+        <div className="section Todo">
           {tasks
             .filter((task) => task.status === "Todo")
             .map((task) => (
               <div key={task.id} className="task">
-                <p className="TaskCard" >{task.name}</p>
+                <p className="TaskCard">{task.name}</p>
               </div>
             ))}
         </div>
       </div>
-      </div>
-    
-    );
-  };
-  
+    </div>
+  );
+};
+
 export default ListTasks;
